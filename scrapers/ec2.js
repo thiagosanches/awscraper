557450edcbae5c14200942ec22be935ca0dab620
Thiago Sanches <tsigwt@gmail.com>
2023-01-18 20:25:41 -0300

    Fetch some simple data from AWS

--- scrapers/ec2.js
+++ scrapers/ec2.js
@@ -1,5 +1,17 @@
+const AWS = require('aws-sdk')
+
 module.exports.scrape = async function () {
     console.log("   Scrapping EC2 data!")
+
+    const ec2 = new AWS.EC2({ apiVersion: '2016-11-15' })
     const data = []
+    const params = { NextToken: null }
+
+    do {
+        const result = await ec2.describeInstances(params).promise()
+        params.NextToken = result.NextToken
+        result.Reservations.forEach(i => data.push(...i.Instances))
+    } while (params.NextToken)
+
     return data
 }
