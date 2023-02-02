6cd379c7826f7ae053af7bdaf52b9a3e847a6ab5
Thiago Sanches <tsigwt@gmail.com>
2023-02-02 16:03:16 -0300

    Add a simple script that returns all the regions

---
+++ utils/awsRegions.js
@@ -0,0 +1,7 @@
+const AWS = require('aws-sdk');
+
+module.exports.getEnabledRegions = async function (account, credentialsParams) {
+    console.log(`🌎 Getting all the enabled regions for Account Id: '${account.Id}'`)
+    const ec2 = new AWS.EC2({ apiVersion: '2016-11-15', region: 'us-east-1', ...credentialsParams })
+    return await ec2.describeRegions({}).promise()
+};
