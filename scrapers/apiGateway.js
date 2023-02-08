39d4a73598b85ab5c40bf1e3a3a1be65cee070d8
Thiago Sanches <tsigwt@gmail.com>
2023-02-08 19:05:20 -0300

    Add APIGateway support

--- scrapers/apiGateway.js
+++ scrapers/apiGateway.js
@@ -1,26 +1,24 @@
 const AWS = require('aws-sdk');
-const mapper = require('../mappers/ec2');
+const mapper = require('../mappers/apiGateway');
 
 module.exports.scrape = async function (account, credentialsParams) {
     console.log('⚙️  Scrapping ApiGateway data!');
 
-    const ag = new AWS.APIGateway({ apiVersion: '2016-11-15', region: account.Region, ...credentialsParams });
+    const ag = new AWS.APIGateway({ apiVersion: '2015-07-09', region: account.Region, ...credentialsParams });
     const data = {
-        type: 'ag', items: [], accountId: account.Id, accountName: account.Name, region: account.Region,
+        type: 'api-gateway', items: [], accountId: account.Id, accountName: account.Name, region: account.Region,
     };
 
     const params = {
         limit: 500,
-        position: null
+        position: null,
     };
 
     do {
         const result = await ag.getRestApis(params).promise();
         params.position = result.position;
 
-        if (result.items)
-            result.items.forEach((i) => data.items.push(i));
-
+        if (result.items) result.items.forEach((i) => data.items.push(i));
     } while (params.position);
 
     const result = await mapper.map(data);
