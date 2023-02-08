a50bed41961b1342e38b78cd4cc74cc844510483
Thiago Sanches <tsigwt@gmail.com>
2023-02-08 17:59:58 -0300

    Add SQS support

--- scrapers/sqs.js
+++ scrapers/sqs.js
@@ -2,19 +2,19 @@ const AWS = require('aws-sdk');
 const mapper = require('../mappers/sqs');
 
 module.exports.scrape = async function (account, credentialsParams) {
-    console.log('⚙️  Scrapping SNS data!');
+    console.log('⚙️  Scrapping SQS data!');
 
-    const sns = new AWS.SQS({ apiVersion: '2012-11-05', region: account.Region, ...credentialsParams });
+    const sqs = new AWS.SQS({ apiVersion: '2012-11-05', region: account.Region, ...credentialsParams });
     const data = {
         type: 'sqs', items: [], accountId: account.Id, accountName: account.Name, region: account.Region,
     };
     const params = { NextToken: null };
 
     do {
-        const result = await sns.listQueues(params).promise();
+        const result = await sqs.listQueues(params).promise();
         params.NextToken = result.NextToken;
         if (result.QueueUrls) {
-            result.Topics.forEach((i) => data.items.push({ Url: i }));
+            result.QueueUrls.forEach((i) => data.items.push({ Url: i }));
         }
     } while (params.NextToken);
 
