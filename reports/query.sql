2e442073c35e37fa169ddb782070c9cf4b677f1f
Thiago Sanches <tsigwt@gmail.com>
2023-02-10 17:43:01 -0300

    Beautify the SQL

--- reports/query.sql
+++ reports/query.sql
@@ -1,2 +1,8 @@
-SELECT json_object("_Id", Id, "_AccountName", AccountName, "_AccountId", AccountId, "_Team", Team, "_Comments", Comments, "_RawObj", json(RawObj)) 
-FROM resources WHERE type = '@type' and status = 'LIVE'
+SELECT json_object(
+    "_Id", Id, 
+    "_AccountName", AccountName, 
+    "_AccountId", AccountId, 
+    "_Team", Team, 
+    "_Comments", Comments, 
+    "_RawObj", json(RawObj)) 
+FROM resources WHERE Type = '@type' AND Status = 'LIVE'
