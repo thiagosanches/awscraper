77f88b71309ea323334a4a6736ec0952ded415f9
Thiago Sanches <thiagom@ciandt.com>
2023-02-10 09:05:06 -0300

    Only consider LIVE resources on the report

--- reports/query.sql
+++ reports/query.sql
@@ -1,2 +1,2 @@
 SELECT json_object("_Id", Id, "_AccountName", AccountName, "_AccountId", AccountId, "_Team", Team, "_Comments", Comments, "_RawObj", json(RawObj)) 
-FROM resources WHERE type = '@type'
+FROM resources WHERE type = '@type' and status = 'LIVE'
