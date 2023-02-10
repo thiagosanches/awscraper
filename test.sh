13c0e3a2997fd8fa1047c852dc8db3f50c9c489d
Thiago Sanches <thiagom@ciandt.com>
2023-02-10 08:09:58 -0300

    Remove node_modules folder from analysys

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@ set -o pipefail
 ERRORS=()
 
 # find all executables and run `shellcheck`
-for f in $(find . -type f -not -path '*.git*' -not -path '*topsecret*' | sort -u); do
+for f in $(find . -name '*.sh' -type f -not -path '*.git*' -not -path '*topsecret*' -not -path '*node_modules*' | sort -u); do
 	if file "$f" | grep --quiet shell; then
 		{
 			shellcheck -x "$f" && echo "[OK]: successfully linted $f"
