4d2c4b114002f49c8a8af999ae1612164651ced7
Thiago Sanches <tsigwt@gmail.com>
2023-02-10 19:31:17 -0300

    Refactor the other report

--- reports/report.sh
+++ reports/report.sh
@@ -21,7 +21,8 @@ function message() {
     echo -e "${GREEN}==========================================================================================${NOCOLOR}"
 }
 
-# Just to avoid repeating the same commands accross the other reports.
+# Just to avoid repeating the same commands accross the other reports,
+# if you need to change the output format, just play around with the commands.
 function format_report_data() {
     cat | sort -u | \
     sed 's/"null"/""/g' | \
@@ -47,50 +48,38 @@ TYPE="ebs"
 sed "s/@type/$TYPE/g" "$QUERY" | sqlite3 "$SQLITEFILE" | 
 jq '[._AccountName, ._AccountId, ._Type, ._Id, ._Team, ._Comments, (._RawObj | select(.Encrypted == false) | .Encrypted) | tostring] | @csv' --raw-output | \
     grep "false" | \
-    sort -u | \
-    sed 's/"null"/""/g' | \
-    column -t -s','
+    format_report_data
 
 message "AWS S3 buckets without encryption"
 TYPE="s3"
 sed "s/@type/$TYPE/g" "$QUERY" | sqlite3 "$SQLITEFILE" | 
 jq '[._AccountName, ._AccountId, ._Type, ._Id, ._Team, ._Comments, (._RawObj | .Encryption) | tostring] | @csv' --raw-output | \
     grep -v "ApplyServerSideEncryptionByDefault" | \
-    sort -u | \
-    sed 's/"null"/""/g' | \
-    column -t -s','
+    format_report_data
 
 message "AWS CloudFront without any WAF (WebACLId)"
 TYPE="cloudfront"
 sed "s/@type/$TYPE/g" "$QUERY" | sqlite3 "$SQLITEFILE" | 
 jq '[._AccountName, ._AccountId, ._Type, ._Team, ._Comments, (._RawObj | .DomainName, .WebACLId, .Aliases.Items[]) | tostring] | @csv' --raw-output | \
     grep -v "arn:aws:wafv2" | \
-    sort -u | \
-    sed 's/"null"/""/g' | \
-    column -t -s','
+    format_report_data
 
 message "AWS SecurityGroups allowing 22 (SSH) to the world (0.0.0.0/0)"
 TYPE="sg"
 sed "s/@type/$TYPE/g" "$QUERY" | sqlite3 "$SQLITEFILE" | 
 jq '[._AccountName, ._AccountId, ._Type, ._Id, ._Team, ._Comments, (._RawObj | .IpPermissions[] | select(.FromPort == 22) | .FromPort, .IpRanges[].CidrIp) | tostring] | @csv' --raw-output | \
     grep "0.0.0.0/0" | \
-    sort -u | \
-    sed 's/"null"/""/g' | \
-    column -t -s','
+    format_report_data
 
 message "AWS EC2 instances"
 TYPE="ec2"
 sed "s/@type/$TYPE/g" "$QUERY" | sqlite3 "$SQLITEFILE" | 
 jq '[._AccountName, ._AccountId, ._Type, ._Id, ._Team, ._Comments, (._RawObj | .Tags[] | select(.Key == "Name") | .Value) | tostring] | @csv' --raw-output | \
-    sort -u | \
-    sed 's/"null"/""/g' | \
-    column -t -s','
+    format_report_data
 
 message "AWS ElasticBeanstalk that are not GREEN"
 TYPE="elastic-beanstalk"
 sed "s/@type/$TYPE/g" "$QUERY" | sqlite3 "$SQLITEFILE" | 
 jq '[._AccountName, ._AccountId, ._Type, ._Id, ._Team, ._Comments, (._RawObj | .EnvironmentName, .SolutionStackName, .Health) | tostring] | @csv' --raw-output | \
-    sort -u | \
-    sed 's/"null"/""/g' | \
     grep -v "Green" | \
-    column -t -s','
+    format_report_data
