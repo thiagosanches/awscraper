f570d9a90943a9a96dfbb3509afa3b3cf7520a60
Thiago Sanches <thiagom@ciandt.com>
2024-10-15 13:19:37 -0300

    Update to add glue

---
+++ scrapers/glue.js
@@ -0,0 +1,23 @@
+const AWS = require('aws-sdk');
+const mapper = require('../mappers/glue');
+
+module.exports.scrape = async function (account, credentialsParams) {
+    console.log('⚙️  Scrapping Glue data!');
+
+    const glue = new AWS.Glue({ apiVersion: '2017-03-31', region: account.Region, ...credentialsParams });
+    const data = {
+        type: 'glue', items: [], accountId: account.Id, accountName: account.Name, region: account.Region,
+    };
+
+    const params = { NextToken: null };
+
+    do {
+        const result = await glue.getJobs(params).promise();
+        console.log(result.Jobs);
+        params.NextToken = result.NextToken;
+        if (result.Jobs) result.Jobs.forEach((i) => data.items.push(i));
+    } while (params.NextToken);
+
+    const result = await mapper.map(data);
+    return result;
+};
