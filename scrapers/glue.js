1cb7fe002812d5661861c0ec7b1ae47dce6b52e2
Thiago Sanches <thiagom@ciandt.com>
2025-02-28 13:49:27 -0300

    Update to get more info on executions for glue jobs

--- scrapers/glue.js
+++ scrapers/glue.js
@@ -13,9 +13,32 @@ module.exports.scrape = async function (account, credentialsParams) {
 
     do {
         const result = await glue.getJobs(params).promise();
-        console.log(result.Jobs);
+
+        for (let i = 0; i < result.Jobs.length; i += 1) {
+            const executions = await glue.getJobRuns({
+                JobName: result.Jobs[i].Name,
+                MaxResults: 2 // to retrieve the last and previous execution results only.
+            }).promise()
+
+            if (executions.JobRuns &&
+                executions.JobRuns.length > 0) {
+                executions.JobRuns[0].ExecutionTimeMinutes = executions.JobRuns[0].ExecutionTime / 60;
+                executions.JobRuns[0].ExecutionTimeHours = executions.JobRuns[0].ExecutionTime / 3600;
+                
+                // PLEASE REFACTOR THIS IS UGLY
+                if (executions.JobRuns[1]) {
+                    executions.JobRuns[1].ExecutionTimeMinutes = executions.JobRuns[1].ExecutionTime / 60;
+                    executions.JobRuns[1].ExecutionTimeHours = executions.JobRuns[1].ExecutionTime / 3600;
+                }
+
+                result.Jobs[i].Executions = executions.JobRuns;
+            }
+        }
+
         params.NextToken = result.NextToken;
-        if (result.Jobs) result.Jobs.forEach((i) => data.items.push(i));
+        if (result.Jobs) {
+            result.Jobs.forEach((i) => data.items.push(i));
+        }
     } while (params.NextToken);
 
     const result = await mapper.map(data);
